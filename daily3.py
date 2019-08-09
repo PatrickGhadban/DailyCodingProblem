6fcffce4278d021eb20f228e835bf4410cb610b9
Patrick Ghadban <pghadban@gmail.com>
2019-08-09 14:26:43 -0400

    Notes

--- daily3.py
+++ daily3.py
@@ -6,6 +6,7 @@ write a function that calculates the maximum profit you could have made from buy
 You must buy before you can sell it.
 For example, given [9, 11, 8, 5, 7, 10], you should return 5, since you could buy the stock at 5 dollars and sell it at 10 dollars.
 
+Time Taken:
 RunTime:
 Space Complexity:
 '''
